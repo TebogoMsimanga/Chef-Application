7a1eb7698a2e4ca8a08f29533c7a71d27cdc0c1e
TebogoMsimanga <st10488599@rcconnect.edu.za>
2025-10-20 11:33:27 +0200

    added fonts and data file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect} from 'react';
+
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    'Quicksand-Light': require('../assets/fonts/Quicksand-Light.ttf'),
+    'Quicksand-Regular': require('../assets/fonts/Quicksand-Regular.ttf'),
+    'Quicksand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
+    'Quicksand-SemiBold': require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    'Quicksand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
+  });
+
+  useEffect(() => {
+   if(error) throw error;
+   if(fontsLoaded) SplashScreen.hideAsync()
+  }, [fontsLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false}}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{
+        fontFamily: 'Quicksand-Bold',
+        fontSize: 40
+      }}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
